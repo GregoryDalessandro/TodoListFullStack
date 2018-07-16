849867d6ff3ddf0973b64a91192f34ac08bb442e
GregoryDalessandro <gregorydalessandro942@gmail.com>
2018-07-16 19:16:43 -0400

    add event on click show all

--- client/src/components/view.js
+++ client/src/components/view.js
@@ -1,7 +1,13 @@
 // console.log('test');
 $(document).ready(function() {
   console.log('document is ready');
+
+
   $('#add').on('click', function() {
     console.log('add button works');
+  });
+
+  $('#showAll').on('click', function() {
+    console.log('show all button works');
   });
 });

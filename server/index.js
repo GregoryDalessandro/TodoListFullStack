7a9242b96de7780a416d85490e8aec28c0e6b2c2
GregoryDalessandro <gregorydalessandro942@gmail.com>
2018-07-16 14:51:20 -0400

    begin scaffolding

---
+++ server/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+// GET - /api/posts (Retrieve all posts)
+app.get('/api/posts', function (req, res) {
+
+});
+
+// POST - /api/posts (Create a new post)
+app.post('/api/posts', function (req, res) {
+
+});
+
+// GET - /api/posts/:id (Retrieve a single post by its id)
+app.get('api/posts/:id', function (req, res) {
+
+});
+
+// PUT - /api/posts/:id (Update a single post by its id)
+app.put('api/posts/:id', function (req, res) {
+
+});
+
+// DELETE - /api/posts/:id (Delete a single post by its id)
+app.delete('/api/posts/:id', function (req, res) {
+
+});
+

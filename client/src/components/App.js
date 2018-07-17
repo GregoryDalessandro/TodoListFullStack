2c3ef1f887bae5d0c46a337c658f1acda1fe98a0
GregoryDalessandro <gregorydalessandro942@gmail.com>
2018-07-17 15:05:17 -0400

    add edit and remove buttons to list items dynamically

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,20 @@
 class TodoListItem extends React.Component {
   constructor(props) {
     super(props);
+    this.onClickEdit = this.onClickEdit.bind(this);
+    this.onClickRemove = this.onClickRemove.bind(this);
   }
+  onClickEdit() {};
+
+  onClickRemove() {};
 
   render() {
     return (
+      <div>
       <li>{this.props.todo}</li>
+      <button type="button" className="edit" onClick={this.onClickEdit}>edit</button>
+      <button type="button" className="remove" onClick={this.onClickRemove}>remove</button>
+      </div>
     );
   }
 

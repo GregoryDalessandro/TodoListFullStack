7fe184cdb39a4430dd142b629b566e513389494b
GregoryDalessandro <gregorydalessandro942@gmail.com>
2018-07-17 13:10:21 -0400

    add hardcoded list items to be rendered

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -13,13 +13,25 @@
 //     )
 //   }
 // }
-
-// ReactDOM.render(
-
-// );
+var TodoList = (props) => (
+  <ul>
+    <li>{props.todos[0]}</li>
+    <li>{props.todos[1]}</li>
+    <li>{props.todos[2]}</li>
+  </ul>
+);
 
 var App = () => (
-  <div>Hello World</div>
+  <div>
+    <h2>Todo List MERN FullStack</h2>
+    <div class="textbox">
+      <input id="msg" type="input"></input>
+      <button id="add">add</button>
+    </div>
+    <TodoList todos={['make a frontend', 'make a server', 'make a backend']}/>
+    <button id="showAll">show all</button>
+  </div>
 );
+
 
 ReactDOM.render(<App />, document.getElementById("app"))

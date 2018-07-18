bfa229440bd62b473d9ee7f90351fabe2baaf30f
GregoryDalessandro <gregorydalessandro942@gmail.com>
2018-07-18 14:50:25 -0400

    refactor App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,25 @@
-var App = () => (
-  <div>
 
-    <h2>Todo List MERN FullStack</h2>
 
-    <Form />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: ['make a frontend','make a server', 'make a backend']
+    }
+  }
 
-    <div>
-      <TodoList todos={['make a frontend','make a server', 'make a backend']}/>
-    </div>
-
-    <button id="showAll">show all</button>
-
-  </div>
-);
-
+  render() {
+    return (
+      <div>
+        <h2>Todo List MERN FullStack</h2>
+        <Form />
+        <div>
+          <TodoList todos={this.state.todos}/>
+        </div>
+        <button id="showAll">show all</button>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("app"))
